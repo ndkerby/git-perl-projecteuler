3664cf814bcd6ef145a2877bcdfd8cc36c4f07f6
Natalie Kerby <nataliekerby@Natalies-MacBook-Pro-2.local>
2013-09-20 22:58:32 -0400

    Adding first thrre projects, need to clean up code

---
+++ Multiples_3_5.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+#
+# Project Euler.net
+# 
+# PROBLEM 1:
+#     If we list all the natural numbers below 10 that are multiples of 3  
+#     or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# ANSWER: 
+#    The sum is: 233168
+#
+# @author
+# Natalie Kerby :: ndkerby@gmail.com
+
+my @numbers = (0 .. 999);
+my $total = 0;
+
+foreach my $number ( @numbers ) 
+{
+    if ( $number%3 == 0 || $number%5 == 0 )
+    {
+        $total += $number;
+    }
+}
+
+print "The sum is: " . $total . "\n";
