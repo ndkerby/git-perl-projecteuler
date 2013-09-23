0056718d316b26156e711b260ece5c5c3e262c53
Natalie Kerby <nataliekerby@Natalies-MacBook-Pro-2.local>
2013-09-22 21:10:31 -0400

    More problems completed

--- Special_Pythagorean_Triplet.pl
+++ Special_Pythagorean_Triplet.pl
@@ -10,12 +10,45 @@
 #     Find the product abc.
 #
 # ANSWER: 
-#    The product of abc is: 
+#    The product of abc is: 31875000
+#    A: 200, B:375, C:245
 #
 # @author
 # Natalie Kerby :: ndkerby@gmail.com
 
 use strict;
 
+my $sum_a_b_c = 1000;
+my $product_a_b_c;
+my $a;
+my $b;
+my $c;
 
-print "The product of abc is: " . $largest_product . "\n";
+#my $c = $sum_a_b_c/2;
+for( my $c = 1 ; $c < $sum_a_b_c ; $c++){
+    for( my $int = 1 ; $int < $sum_a_b_c-$c ; $int++){
+        $a = $int;
+        $b = $sum_a_b_c-$c-$int;
+        my $temp = isPythagorean($a, $b, $c);
+        if ($temp){
+            $product_a_b_c = $a * $b * $c;
+            my $sum = $a+$b+$c;
+            print "The sum of abc is: " . $sum . "\n";
+            print "The product of abc is: " . $product_a_b_c . "\n";
+            print $a . ", " . $b . ", " . $c . "\n";
+            exit(0);
+        }
+    }
+}
+print "Not Found\n";
+
+sub isPythagorean{
+    my ($currentA, $currentB, $currentC ) = @_;
+    #print $currentA . ", " . $currentB . ", " . $currentC . "\n";
+    my $isPythagorean = 0;
+    if (($currentA**2 + $currentB**2) eq $currentC**2){
+            $isPythagorean = 1;
+            return $isPythagorean;
+    } 
+    return $isPythagorean;
+}
